2792ba9a9d4521b11749ff20683cbb88792017fa
冯淇 <feengqi@gmail.com>
2021-01-11 11:45:18 +0800

    init🎉

---
+++ README.md
@@ -0,0 +1,1 @@
+- 使用@umijs/plugin-qiankun，主应用和子应用都为umi项目
---
+++ t1/.umirc.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    {
+      path: '/',
+      component: '@/pages/index',
+    },
+    {
+      path: '/app1',
+      microApp: 'app1',
+    },
+  ],
+  qiankun: {
+    master: {
+      // 注册子应用信息
+      apps: [
+        {
+          name: 'app1', // 唯一 id
+          entry: '//localhost:7001', // html entry
+        },
+      ],
+    },
+  },
+});
